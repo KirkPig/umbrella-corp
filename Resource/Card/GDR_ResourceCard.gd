e36c47354eb40460c9552b2cad23a660e99ea3fc
Kirk Pig <kriklomark@gmail.com>
2024-11-27 21:47:38 +0700

    feat: added upgrade data in resource card data

--- Resource/Card/GDR_ResourceCard.gd
+++ Resource/Card/GDR_ResourceCard.gd
@@ -23,5 +23,13 @@ enum EBusiness{
 @export_category("Research")
 @export var research_yield: Array[ResearchReward]
 
+@export_category("Upgrade")
+@export var upgrade_yield_amount: int = 1
+@export var upgrade_yield_gold: int = 10
+@export var upgrade_price_amount: int = 10
+@export var upgrade_price_gold: int = 10
+@export var upgrade_demand_amount: int = 1
+@export var upgrade_demand_gold: int = 10
+
 func get_business_card_data() -> BusinessCardData:
 	return CardManager.card_dict[business_id]
