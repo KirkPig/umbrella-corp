8cc96b03f81822e48b1be1d911ea938c50bee92a
oJadeo <jade.jiravate@gmail.com>
2024-12-01 10:46:34 +0700

    feat: card f;ash

--- UI/ToolTip/GD_ToolTipsKeyword.gd
+++ UI/ToolTip/GD_ToolTipsKeyword.gd
@@ -29,7 +29,7 @@ var resource_id:int = -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	self.size_flags_horizontal = Control.SIZE_SHRINK_END
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
